867d5731d47d6178bcc8e8d3de636bced34da579
bhanu <bhanusree4488@gmail.com>
2026-07-13 23:39:10 +0530

    odd

--- first.java
+++ first.java
@@ -1,9 +1,18 @@
-public class First {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        System.out.println("The world is my oyster");
-        System.out.println("hello");
-        System.out.println("hi");
-        System.out.println("The world is my oyster");
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+
+        sc.close();
     }
 }
